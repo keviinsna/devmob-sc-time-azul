6d8f2f0b1967cac37a98436a4ad906a9ef13fa96
Nicholas Figueira <nicholasaf@dcc.ufrj.br>
2023-09-30 12:04:49 -0300

    feature: criado fale-conosco html

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById("submit-button").onclick = function(){
+    alert("E-mail enviado! Entraremos em contanto em breve!")
+}
+// Não tem como tirar o título do site sem biblioteca externa
+// Deve-se decidir se mantem isso ou não
