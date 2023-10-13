2829cdfe9b57d9a3902449c79ffb4ef869dbdd47
keviinsna <keevinsenna2001@gmail.com>
2023-10-13 10:59:14 -0300

    altera layout sessoes

---
+++ pages/sessoes/script.js
@@ -0,0 +1,424 @@
+const json = {
+  palestras: [
+    {
+      titulo: "Indústria 4.0: Transformação Digital na Manufatura",
+      descricao:
+        "Explorando casos de sucesso e desafios na adoção da Indústria 4.0. Os palestrantes apresentarão as tecnologias-chave, como IoT e automação, que impulsionam a transformação digital na manufatura.",
+      data: "16 OUT",
+      hora: "09:45",
+      duracao: "1h 15min",
+      local: "Centro de Inovação Industrial",
+      palestrantes: [
+        {
+          nome: "Guilherme Oliveira",
+          cargo: "Consultor de Inovação Industrial",
+          empresa: "InovaManuf Ltda.",
+        },
+        {
+          nome: "Mariana Silva",
+          cargo: "CEO e Fundador",
+          empresa: "TechRevolution Manufacturing",
+        },
+      ],
+    },
+    {
+      titulo: "Desenvolvimento de Aplicações VR/AR",
+      descricao:
+        "Uma exploração prática e inspiradora sobre o desenvolvimento de aplicações de Realidade Virtual e Aumentada. Os palestrantes compartilharão casos de sucesso, desafios superados e as últimas tendências nesse emocionante campo tecnológico.",
+      data: "16 OUT",
+      hora: "10:30",
+      duracao: "1h 30min",
+      local: "Auditório Virtual Reality",
+      palestrantes: [
+        {
+          nome: "Camila Santos",
+          cargo: "Engenheiro de Realidade Virtual",
+          empresa: "Tech Innovate VR",
+        },
+        {
+          nome: "Rafael Lima",
+          cargo: "Designer de Experiência XR",
+          empresa: "Studio Immersion",
+        },
+      ],
+    },
+    {
+      titulo: "Internet das Coisas (IoT): Conectando o Mundo",
+      descricao:
+        "Explorando o impacto da Internet das Coisas (IoT) na vida cotidiana e nos setores industriais. Os palestrantes abordarão casos de uso, desafios e as perspectivas futuras dessa revolucionária tecnologia.",
+      data: "16 OUT",
+      hora: "14:00",
+      duracao: "1h",
+      local: "Sala IoT World",
+      palestrantes: [
+        {
+          nome: "Larissa Oliveira",
+          cargo: "Especialista em IoT",
+          empresa: "IoT Solutions Inc.",
+        },
+        {
+          nome: "Pedro Rocha",
+          cargo: "Engenheiro de Sistemas",
+          empresa: "Connected Devices Co.",
+        },
+      ],
+    },
+    {
+      titulo: "Robótica Avançada: Além da Linha de Montagem",
+      descricao:
+        "Explorando as últimas inovações em robótica avançada e suas aplicações além da linha de montagem industrial. Os palestrantes discutirão robôs autônomos, robótica colaborativa e o impacto na sociedade.",
+      data: "18 OUT",
+      hora: "13:45",
+      duracao: "1h 15min",
+      local: "Sala RoboTech",
+      palestrantes: [
+        {
+          nome: "André Silva",
+          cargo: "Engenheiro de Controle Robótico",
+          empresa: "RoboInnovation Ltda.",
+        },
+        {
+          nome: "Mariana Oliveira",
+          cargo: "Pesquisador em Inteligência de Robôs",
+          empresa: "TechRobo Research Institute",
+        },
+      ],
+    },
+    {
+      titulo: "Inovações em Inteligência Artificial",
+      descricao:
+        "Explorando as últimas tendências e avanços em Inteligência Artificial, com foco em aplicações práticas, aprendizado profundo e impacto na sociedade. Os palestrantes compartilharão insights valiosos sobre as inovações que estão moldando o futuro da IA.",
+      data: "18 OUT",
+      hora: "14:00",
+      duracao: "1h 30min",
+      local: "Centro de Convenções TechHub",
+      palestrantes: [
+        {
+          nome: "Diana Silva",
+          cargo: "Pesquisador Sênior em IA",
+          empresa: "Instituto de Pesquisa Avançada",
+        },
+        {
+          nome: "Carlos Oliveira",
+          cargo: "Professor de Ciência da Computação",
+          empresa: "Universidade TechMaster",
+        },
+      ],
+    },
+    {
+      titulo: "Tecnologias Emergentes: Da Teoria à Prática",
+      descricao:
+        "Uma visão abrangente de como as tecnologias emergentes estão moldando o futuro. Os palestrantes irão discutir desde inteligência artificial até biotecnologia, explorando as interseções entre essas inovações e seu impacto na sociedade.",
+      data: "18 OUT",
+      hora: "14:30",
+      duracao: "1h 15min",
+      local: "Sala Futurista",
+      palestrantes: [
+        {
+          nome: "Bruno Rocha",
+          cargo: "Estrategista de Inovação",
+          empresa: "FutureTech Insights",
+        },
+        {
+          nome: "Laura Santos",
+          cargo: "Pesquisador em Tecnologias Emergentes",
+          empresa: "Instituto de Pesquisa TechForward",
+        },
+      ],
+    },
+    {
+      titulo: "Realidade Aumentada: Transformando Experiências",
+      descricao:
+        "Uma imersão no mundo da Realidade Aumentada (AR) e suas aplicações em diferentes setores. Os palestrantes compartilharão casos práticos, tendências e o potencial transformador da AR na forma como interagimos com o ambiente.",
+      data: "17 OUT",
+      hora: "10:30",
+      duracao: "1h",
+      local: "Auditório AR Experience",
+      palestrantes: [
+        {
+          nome: "Fernando Lima",
+          cargo: "Desenvolvedor AR",
+          empresa: "AR Innovate Tech",
+        },
+        {
+          nome: "Carla Santos",
+          cargo: "Designer de Experiência AR",
+          empresa: "Immersive Designs Co.",
+        },
+      ],
+    },
+    {
+      titulo: "5G e a Revolução da Conectividade",
+      descricao:
+        "Uma análise profunda do impacto do 5G nas comunicações e na sociedade. Os palestrantes abordarão as inovações trazidas pelo 5G, desde a velocidade da conectividade até as aplicações transformadoras em diferentes setores.",
+      data: "17 OUT",
+      hora: "16:20",
+      duracao: "1h 15min",
+      local: "Sala ConectaTech",
+      palestrantes: [
+        {
+          nome: "Ricardo Alves",
+          cargo: "Engenheiro de Telecomunicações 5G",
+          empresa: "Connectivity Innovate",
+        },
+        {
+          nome: "Ana Oliveira",
+          cargo: "Estrategista de Redes",
+          empresa: "GlobalConnect Solutions",
+        },
+      ],
+    },
+    {
+      titulo: "Sustentabilidade na Tecnologia",
+      descricao:
+        "Explorando como a tecnologia pode impulsionar práticas sustentáveis para um futuro mais verde. Os palestrantes compartilharão iniciativas inovadoras e estratégias para integrar a sustentabilidade no desenvolvimento tecnológico.",
+      data: "20 OUT",
+      hora: "17:00",
+      duracao: "1h 30min",
+      local: "Sala SustenTech",
+      palestrantes: [
+        {
+          nome: "Gabriel Oliveira",
+          cargo: "Consultor em Tecnologia Sustentável",
+          empresa: "GreenTech Consulting",
+        },
+        {
+          nome: "Amanda Almeida",
+          cargo: "Empreendedor em Sustentabilidade",
+          empresa: "Sustainable Innovations Ltda.",
+        },
+      ],
+    },
+    {
+      titulo: "Futuro da Educação: Tecnologia na Sala de Aula",
+      descricao:
+        "Abordando a integração de tecnologia na educação e como ela está moldando o futuro da aprendizagem. Os palestrantes discutirão plataformas educacionais inovadoras, realidade virtual na educação e tendências em EdTech.",
+      data: "19 OUT",
+      hora: "15:15",
+      duracao: "1h 15min",
+      local: "Auditório EdTech",
+      palestrantes: [
+        {
+          nome: "Gabriela Almeida",
+          cargo: "Especialista em EdTech",
+          empresa: "EduInnovate Solutions",
+        },
+        {
+          nome: "Pedro Santos",
+          cargo: "Professor de Tecnologia Educacional",
+          empresa: "Universidade EduTech",
+        },
+      ],
+    },
+    {
+      titulo: "Cibersegurança: Desafios e Soluções",
+      descricao:
+        "Uma análise aprofundada das últimas ameaças cibernéticas e estratégias de defesa. Os palestrantes compartilharão insights sobre as tendências em cibersegurança, abordando desde ataques comuns até técnicas avançadas de defesa.",
+      data: "19 OUT",
+      hora: "15:15",
+      duracao: "1h",
+      local: "Sala SeguraNet",
+      palestrantes: [
+        {
+          nome: "Patrícia Oliveira",
+          cargo: "Especialista em Cibersegurança",
+          empresa: "CyberGuard Solutions",
+        },
+        {
+          nome: "Lucas Silva",
+          cargo: "Pesquisador em Ética Hacking",
+          empresa: "HackerEthics Labs",
+        },
+      ],
+    },
+    {
+      titulo: "O Futuro da Computação Quântica",
+      descricao:
+        "Uma exploração fascinante das perspectivas e avanços na computação quântica. Os palestrantes apresentarão os princípios fundamentais, os desafios atuais e as possíveis aplicações práticas dessa revolucionária tecnologia.",
+      data: "20 OUT",
+      hora: "11:00",
+      duracao: "1h 30min",
+      local: "Auditório Quântico",
+      palestrantes: [
+        {
+          nome: "Roberto Almeida",
+          cargo: "Pesquisador em Computação Quântica",
+          empresa: "Instituto de Tecnologia Quântica",
+        },
+        {
+          nome: "Fernanda Lima",
+          cargo: "Engenheiro de Sistemas Quânticos",
+          empresa: "QuantumTech Solutions",
+        },
+      ],
+    },
+    {
+      titulo: "Inteligência Artificial na Medicina",
+      descricao:
+        "Explorando as aplicações e avanços da inteligência artificial na área da saúde. Os palestrantes discutirão diagnóstico médico, análise de dados clínicos e o impacto da IA na personalização do tratamento.",
+      data: "20 OUT",
+      hora: "11:00",
+      duracao: "1h 15min",
+      local: "Sala MedTech AI",
+      palestrantes: [
+        {
+          nome: "Roberta Oliveira",
+          cargo: "Pesquisador em IA na Saúde",
+          empresa: "MedAI Research Center",
+        },
+        {
+          nome: "José Lima",
+          cargo: "Médico Especialista em Inteligência Artificial",
+          empresa: "HealthTech Clinic",
+        },
+      ],
+    },
+    {
+      titulo: "Segurança Digital: Tendências e Desafios",
+      descricao:
+        "Uma análise abrangente das tendências e desafios em segurança digital. Os palestrantes discutirão ameaças emergentes, estratégias de prevenção e o papel da inteligência artificial na detecção de ataques cibernéticos.",
+      data: "16 OUT",
+      hora: "14:30",
+      duracao: "1h 30min",
+      local: "Sala SecureTech",
+      palestrantes: [
+        {
+          nome: "Luciana Silva",
+          cargo: "Especialista em Segurança Digital",
+          empresa: "SecureGuard Solutions",
+        },
+        {
+          nome: "Rafael Oliveira",
+          cargo: "Pesquisador em Ciberameaças",
+          empresa: "CyberDefense Institute",
+        },
+      ],
+    },
+  ],
+};
+
+/**
+ * Renderizando as abas
+ */
+const datas = Array.from(new Set(json.palestras.map((p) => p.data).sort()));
+const tabs = document.getElementsByClassName("tabs")[0];
+datas.forEach((data, idx) => {
+  const button = document.createElement("button");
+  button.type = "button";
+  button.className = "tab";
+
+  if (idx == 0) button.className += " active";
+
+  button.onclick = (evt) => mostrarPalestras(evt, data);
+  button.append(data);
+  tabs.appendChild(button);
+});
+
+/**
+ * Renderizando as palestras
+ */
+const cards = document.getElementsByClassName("tabsContent")[0];
+json.palestras.forEach((p) => {
+  const card = cardPalestra(p);
+  if (p.data != "16 OUT") {
+    card.style.display = "none";
+  }
+  cards.appendChild(card);
+});
+
+function mostrarPalestras(evt, aba) {
+  // Marca aba atual como ativa e desativa as outras
+  const abas = document.getElementsByClassName("tab");
+  for (i = 0; i < abas.length; i++) {
+    abas[i].className = abas[i].className.replace(" active", "");
+  }
+  evt.currentTarget.className += " active";
+
+  // Esconde todos os cards
+  const cards = document.getElementsByClassName("card");
+  for (i = 0; i < cards.length; i++) {
+    cards[i].style.display = "none";
+  }
+
+  // Mostra os cards da aba selecionada
+  json.palestras.forEach((p, idx) => {
+    if (p.data == aba) cards[idx].style.display = "block";
+  });
+}
+
+function cardPalestra(palestra) {
+  // card
+  const article = document.createElement("article");
+  article.className = "card";
+
+  // card header
+  const header = document.createElement("header");
+  const title = document.createElement("h1");
+  title.append(palestra.titulo);
+  // hedaer
+
+  const divIcons = document.createElement("div");
+  divIcons.className = "icones";
+
+  // span
+  const spanHora = document.createElement("span");
+  const spanLocal = document.createElement("span");
+  const spanDuracao = document.createElement("span");
+
+  // ícones
+  const iconeHora = document.createElement("i");
+  iconeHora.className = "fa-solid fa-clock";
+  const iconeLocal = document.createElement("i");
+  iconeLocal.className = "fa-solid fa-location-dot";
+  const iconeDuracao = document.createElement("i");
+  iconeDuracao.className = "fa-solid fa-hourglass";
+
+  spanHora.append(iconeHora, palestra.hora);
+  spanLocal.append(iconeLocal, palestra.local);
+  spanDuracao.append(iconeDuracao, palestra.duracao);
+
+  // main
+  const main = document.createElement("main");
+  const p = document.createElement("p");
+  p.append(palestra.descricao);
+
+  const containerPalestrantes = document.createElement("div");
+  containerPalestrantes.className = "containerPalestrantes";
+  palestra.palestrantes.forEach((p) => {
+    const _palestrante = palestrante(p);
+    containerPalestrantes.append(_palestrante);
+  });
+
+  // Append tags
+  divIcons.append(spanHora, spanLocal, spanDuracao);
+  header.append(divIcons, title);
+
+  main.append(p);
+  article.append(header, main, containerPalestrantes);
+
+  return article;
+}
+
+function palestrante(_palestrante) {
+  const divPalestrante = document.createElement("div");
+  divPalestrante.className = "palestrante";
+
+  const img = document.createElement("img");
+  img.src = "../../images/palestrantes/user.png";
+
+  const infos = document.createElement("div");
+  infos.className = "infos";
+
+  const nome = document.createElement("span");
+  const cargo = document.createElement("span");
+  const empresa = document.createElement("span");
+
+  nome.append(_palestrante.nome);
+  cargo.append(_palestrante.cargo);
+  empresa.append(_palestrante.empresa);
+
+  infos.append(nome, cargo, empresa);
+  divPalestrante.append(img, infos);
+
+  return divPalestrante;
+}
